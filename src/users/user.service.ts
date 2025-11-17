5bdbdc8fc57fcb2d9c821f15a4955c92c7eb1327
mor mbathie <mormbathie98@gmail.com>
2025-11-17 23:21:38 +0100

    creation du table users

---
+++ src/users/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { User } from "./entites/user.entityt";
+import { CreateUserDto } from "./dto/create-user.dto";
+
+@Injectable()
+export class UserService {
+   private users: User[] = [];
+   private nextId = 1;
+   
+   createUser(createUserDto: CreateUserDto): User{
+         const newUser: User = {    
+            id: this.nextId++,
+            name: createUserDto.name,
+            email: createUserDto.email,
+            isActive: createUserDto.isActive,
+         };
+         this.users.push(newUser);
+         return newUser;
+
+   }
+}
